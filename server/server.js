90fbdc82e6b9d9e1ad999d49326ff5525ee77edf
Justin Barner <justinrb14@gmail.com>
2022-02-12 23:35:47 -0800

    fixes

--- server/server.js
+++ server/server.js
@@ -32,5 +32,5 @@ app.get('*', (req, res)=>{
 })
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}/graphql`));
 });
